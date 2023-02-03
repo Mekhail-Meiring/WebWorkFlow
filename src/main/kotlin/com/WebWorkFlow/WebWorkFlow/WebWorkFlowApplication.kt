860b291a928b57f3e731a9440f0e8229991e7aef
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-02-03 19:24:19 +0200

    Created Spring application for web work flow

---
+++ src/main/kotlin/com/WebWorkFlow/WebWorkFlow/WebWorkFlowApplication.kt
@@ -0,0 +1,11 @@
+package com.WebWorkFlow.WebWorkFlow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebWorkFlowApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebWorkFlowApplication>(*args)
+}
---
+++ src/test/kotlin/com/WebWorkFlow/WebWorkFlow/WebWorkFlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.WebWorkFlow.WebWorkFlow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebWorkFlowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
