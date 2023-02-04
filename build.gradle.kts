1927aa80b820d8ae7fe32612a17bb9b73a369811
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-02-04 12:12:25 +0200

    Dependency to read Excel files

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.apache.poi:poi:5.2.3")
+	implementation("org.apache.poi:poi-ooxml:5.2.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
