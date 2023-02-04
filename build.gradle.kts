32287be2aab7831284ffef083a8507e87c0e9b79
mmeiring021 <mmeiring021@student.wethinkcode.co.za>
2023-02-04 16:00:53 +0200

    Start of front end. added gradle plugins to build react app when you start the application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import com.github.gradle.node.task.NodeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("com.github.node-gradle.node") version "3.5.1"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 }
@@ -10,6 +12,7 @@ plugins {
 group = "com.WebWorkFlow"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+
 
 repositories {
 	mavenCentral()
@@ -26,13 +29,54 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+node {
+	download.set(true)
+	version.set("16.13.0")
+	yarnVersion.set("1.27.17")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+
+tasks{
+
+	withType<Test> {
+		useJUnitPlatform()
+	}
+
+	val install = create<NodeTask> ("install dependencies") {
+		workingDir.set(file("${project.projectDir}/src/main/frontend"))
+		args.set(listOf("install"))
+	}
+
+	val build = create<NodeTask> ("build frontend") {
+		dependsOn(install)
+		workingDir.set(file("${project.projectDir}/src/main/frontend"))
+		args.set(listOf("build"))
+	}
+
+	val copy = create<Copy> ("copy frontend") {
+		dependsOn(build)
+		from(file("${project.projectDir}/src/main/frontend/build"))
+		into(file("${rootDir}/build/resources/main/static/."))
+//		into(file("${project.projectDir}/src/main/resources/static"))
+	}
+
+	val cleanup = create<Delete> ("cleanup frontend") {
+		delete(file("${project.projectDir}/src/main/frontend/build"))
+	}
+
+	withType<KotlinCompile> {
+		dependsOn(copy)
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "17"
+		}
+	}
+
+	clean {
+		dependsOn(cleanup)
+	}
+
+
+
 }
+
